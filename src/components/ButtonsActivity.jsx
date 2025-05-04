69b7b74486e4a1b53facf79c030d642afc82dbe2
! Jajou <jahelgithub64@gmail.com>
2025-05-04 12:41:38 +0200

    Update ButtonsActivity.jsx

--- src/components/ButtonsActivity.jsx
+++ src/components/ButtonsActivity.jsx
@@ -6,7 +6,7 @@ function ButtonsActivity({ content }) {
     <p
       onClick={() => setIsClick(!isClick)}
       className={`border border-[#D6E2F5] bg-white p-2 rounded-2xl shadow-lg cursor-pointer transition-colors duration-200 ${
-        isClick ? "bg-orange-700 text-white dark:text-black" : "bg-white text-black dark:bg-black dark:text-white"
+        isClick ? "bg-orange-700 text-black dark:text-black" : "bg-orange-500 text-black dark:bg-orange-500 dark:text-black"
       }`}
     >
       {content}
